6c84a09e6f5aaac8dcdf25fdcfc08b74f3a15455
ehsia1 <echsia16@gmail.com>
2017-09-10 14:04:52 -0400

    Add few more tests

--- lab_1b/submission.py
+++ lab_1b/submission.py
@@ -8,24 +8,28 @@ def basicUnitTest():
     packet1Test = Packets.RequestForgotPasswordPacket.Deserialize(packet1Bytes)
     assert packet1 == packet1Test
 
+    # Security Question Packet test
     packet2 = Packets.SecurityQuestionPacket()
     packet2.securityQuestion = 'What was your hometown?'
     packet2Bytes = packet2.__serialize__()
     packet2Test = Packets.SecurityQuestionPacket.Deserialize(packet2Bytes)
     assert packet2 == packet2Test
 
+    # Security Answer Packet test
     packet3 = Packets.SecurityAnswerPacket()
     packet3.securityAnswer = 'Windsor, CT'
     packet3Bytes = packet3.__serialize__()
     packet3Test = Packets.SecurityAnswerPacket.Deserialize(packet3Bytes)
     assert packet3 == packet3Test
 
+    # Token test
     packet4 = Packets.ForgotPasswordTokenPacket()
     packet4.token = 'asdf2313241SqwerXq'
     packet4Bytes = packet4.__serialize__()
     packet4Test = Packets.ForgotPasswordTokenPacket.Deserialize(packet4Bytes)
     assert packet4 == packet4Test
 
+    # Reset Password Input test
     packet5 = Packets.ResetPasswordInputPacket()
     packet5.newPassword = 'gronkgronkgronk'
     packet5.passwordConfirmation = 'gronkgronkgronk'
@@ -33,24 +37,30 @@ def basicUnitTest():
     packet5Test = Packets.ResetPasswordInputPacket.Deserialize(packet5Bytes)
     assert packet5 == packet5Test
 
+    # Password reset verification test
     packet6 = Packets.PasswordResetPacket()
     packet6.verification = True
     packet6Bytes = packet6.__serialize__()
     packet6Test = Packets.PasswordResetPacket.Deserialize(packet6Bytes)
     assert packet6 == packet6Test
 
+    # test newpassword match passwordConfirmation
     packet7 = Packets.ResetPasswordInputPacket()
     packet7.newPassword = 'gronkgronkgronk'
     packet7.passwordConfirmation = 'gronkgronk'
     packet7Bytes = packet7.__serialize__()
     packet7Test = Packets.ResetPasswordInputPacket.Deserialize(packet7Bytes)
     assert packet7 == packet7Test
+    assert packet7Test.newPassword != packet7Test.passwordConfirmation
 
+    # password reset false test
     packet8 = Packets.PasswordResetPacket()
     packet8.verification = False
     packet8Bytes = packet8.__serialize__()
     packet8Test = Packets.PasswordResetPacket.Deserialize(packet8Bytes)
     assert packet8 == packet8Test
+
+    print("All tests passed!")
 
 if __name__=="__main__":
     basicUnitTest()
