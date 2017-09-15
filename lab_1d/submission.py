1d2464af4e0f0b8462f529be04f634c82b693cd3
ehsia1 <echsia16@gmail.com>
2017-09-15 12:24:35 -0400

    Used playground test case as guidance

--- lab_1d/submission.py
+++ lab_1d/submission.py
@@ -1,12 +1,25 @@
 import Protocol
 import Packets
 import playground
+import asyncio
+# from playground.network.testing import MockTransportToStorageStream, MockTransportToProtocol
 
 def protocolController():
+    loop = asyncio.get_event_loop()
     client = Protocol.ForgotPasswordClientProtocol()
     server = Protocol.ForgotPasswordServerProtocol()
-    playground.getConnector().create_playground_server(server, 2020)
-    playground.getConnector().create_playground_connection(client, 20174.0.0.1 ,2020)
+    # cTransport, sTransport = MockTransportToProtocol.CreateTransportPair(client, server)
+    serv = playground.getConnector().create_playground_server(lambda: server, 8080)
+    s = loop.run_until_complete(serv)
+    print("Echo server running at {}".format(s.sockets[0].gethostname()))
+    cli = playground.getConnector().create_playground_connection (lambda: client, "20174.1.1.1", 8080)
+    transport, protocol = loop.run_until_complete(cli)
+    print("Echo client running at t:{} and p:{}".format(transport, protocol))
+    # server.connection_made(sTransport)
+    # client.connection_made(cTransport)
+    # client.send_initial_message()
+    loop.run_forever()
+    loop.close()
 
 if __name__ == "__main__":
     protocolController()
