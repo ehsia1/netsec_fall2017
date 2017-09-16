bacdef07d831c8aa4e3e798c7ae61b2c4a230d24
ehsia1 <echsia16@gmail.com>
2017-09-16 17:27:07 -0400

    WIP: got the passthrough working, need to pass transport and data

--- lab_1e/PassThrough.py
+++ lab_1e/PassThrough.py
@@ -7,28 +7,28 @@ class PassThrough1(StackingProtocol):
         self.transport = None
         self.count = 0
 
-    def setHigherProtocol(self, higherProtocol):
+    def setHigherProtocol(self, higherProtocol=Protocol.ForgotPasswordServerProtocol()):
         self._higherProtocol = higherProtocol
         if self.count == 0:
-            self._higherProtocol.connection_made()
+            self.higherProtocol().connection_made()
             print("Connection made.")
         elif self.count < 3:
-            self._higherProtocol.data_received()
+            self.higherProtocol().data_received()
         else:
-            self.connection_lost()
+            self.higherProtocol().connection_lost()
         self.count += 1
 
 class PassThrough2(StackingProtocol):
     def __init__(self):
         self.transport = None
 
-    def setHigherProtocol(self, higherProtocol):
+    def setHigherProtocol(self, higherProtocol=Protocol.ForgotPasswordClientProtocol()):
         self._higherProtocol = higherProtocol
         if self.count == 0:
-            self._higherProtocol.connection_made()
+            self.higherProtocol().connection_made()
             print("Connection made.")
         elif self.count < 3:
-            self._higherProtocol.data_received()
+            self.higherProtocol().data_received()
         else:
-            self.connection_lost()
+            self.higherProtocol().connection_lost()
         self.count += 1
