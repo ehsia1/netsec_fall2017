f2ccfb5f198682c8761728b746049b458b5875c8
ehsia1 <echsia16@gmail.com>
2017-09-16 16:15:37 -0400

    Main .py file

---
+++ lab_1e/submission.py
@@ -0,0 +1,26 @@
+import Protocol
+import Packets
+import playground
+import asyncio
+from playground.network.common import StackingProtocolFactory
+from PassThrough import PassThrough1, PassThrough2
+
+def protocolController():
+    loop = asyncio.get_event_loop()
+    loop.set_debug(enabled=True)
+    f = StackingProtocolFactory(lambda: PassThrough1(), lambda: PassThrough2())
+    ptConnector = playground.Connector(protocolStack=f)
+    playground.setConnector("passthrough", ptConnector)
+    client = Protocol.ForgotPasswordClientProtocol()
+    server = Protocol.ForgotPasswordServerProtocol()
+    serv = playground.getConnector('passthrough').create_playground_server(lambda: server, 8080)
+    s = loop.run_until_complete(serv)
+    print("Echo server running at {}".format(s.sockets[0].gethostname()))
+    cli = playground.getConnector().create_playground_connection(lambda: client, "20174.1.1.1", 8080)
+    transport, protocol = loop.run_until_complete(cli)
+    print("Echo client running with t:{}. p:{}".format(transport, protocol))
+    loop.run_forever()
+    loop.close()
+
+if __name__ == "__main__":
+    protocolController()
