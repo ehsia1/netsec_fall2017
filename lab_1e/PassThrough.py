09bc13b7599e92f993bc39c37a5470547386c4a5
ehsia1 <echsia16@gmail.com>
2017-09-16 17:03:01 -0400

    Preliminary setup for higher protocol calls

--- lab_1e/PassThrough.py
+++ lab_1e/PassThrough.py
@@ -5,7 +5,30 @@ from playground.network.common import StackingProtocol
 class PassThrough1(StackingProtocol):
     def __init__(self):
         self.transport = None
+        self.count = 0
+
+    def setHigherProtocol(self, higherProtocol):
+        self._higherProtocol = higherProtocol
+        if self.count == 0:
+            self._higherProtocol.connection_made()
+            print("Connection made.")
+        elif self.count < 3:
+            self._higherProtocol.data_received()
+        else:
+            self.connection_lost()
+        self.count += 1
 
 class PassThrough2(StackingProtocol):
     def __init__(self):
         self.transport = None
+
+    def setHigherProtocol(self, higherProtocol):
+        self._higherProtocol = higherProtocol
+        if self.count == 0:
+            self._higherProtocol.connection_made()
+            print("Connection made.")
+        elif self.count < 3:
+            self._higherProtocol.data_received()
+        else:
+            self.connection_lost()
+        self.count += 1
